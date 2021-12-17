e432aec7103ed5de78e32fb9f69042178a9d8ac5
Julie Qiu <julie@golang.org>
2021-12-17 14:59:39 -0500

    new-vuln.sh: delete
    The script is not used for anything.
    
    Change-Id: Iff976eeefae01bc7f8623e7cb69370c9312e4e2e
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/373154
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- new-vuln.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2021 The Go Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
-
-prev=$(find reports/GO-* | tail -n 1 | sed -n 's/reports\/GO-[0-9]*-\([0-9]*\).yaml/\1/p')
-new=$(printf "%04d" $(expr $prev + 1))
-year=$(date +"%Y")
-cp template reports/GO-$year-$new.yaml
