733040acc3a24726e6f0f79e5dd22dcc3750e189
Julie Qiu <julie@golang.org>
2021-12-20 17:14:01 -0500

    tests: merge lint_test.go and all_test.go
    TestLintReports is moved to all_test.go to consolidate files.
    
    Change-Id: If656af2136c755e29eb8e9171e8e7a341013a825
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/373501
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- all_test.go
+++ all_test.go
@@ -8,12 +8,18 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 	"testing"
+
+	"golang.org/x/vulndb/internal/report"
 )
 
-func Test(t *testing.T) {
+func TestChecksBash(t *testing.T) {
 	bash, err := exec.LookPath("bash")
 	if err != nil {
 		t.Skipf("skipping: %v", err)
@@ -24,5 +30,35 @@ func Test(t *testing.T) {
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		t.Fatal(err)
+	}
+}
+
+const reportsDir = "reports"
+
+func TestLintReports(t *testing.T) {
+	if runtime.GOOS == "js" {
+		t.Skipf("wasm builder does not have network access")
+	}
+	if runtime.GOOS == "android" {
+		t.Skipf("android builder does not have access to reports/")
+	}
+	reports, err := ioutil.ReadDir(reportsDir)
+	if err != nil {
+		t.Fatalf("unable to read reports/: %s", err)
+	}
+	for _, rf := range reports {
+		if rf.IsDir() {
+			continue
+		}
+		t.Run(rf.Name(), func(t *testing.T) {
+			fn := filepath.Join(reportsDir, rf.Name())
+			lints, err := report.LintFile(fn)
+			if err != nil {
+				t.Fatalf("vulnlint.LintReport(%q): %s", fn, err)
+			}
+			if len(lints) > 0 {
+				t.Errorf(strings.Join(lints, "\n"))
+			}
+		})
 	}
 }
