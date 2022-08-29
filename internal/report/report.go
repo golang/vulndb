4fc95cedcda7082a1c74e930eefa2de0c5c6f994
Damien Neil <dneil@google.com>
2022-08-29 10:08:47 -0700

    internal/report: remove support for the old links field
    All reports have been converted to the new references field.
    
    Change-Id: I0bbf1e533a1e3f0a544591d1f98a25c791ef30c0
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/426475
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Tatiana Bradley <tatiana@golang.org>
    Run-TryBot: Damien Neil <dneil@google.com>

--- internal/report/report.go
+++ internal/report/report.go
@@ -89,13 +89,6 @@ type LegacyPackage struct {
 	VulnerableAt Version `yaml:"vulnerable_at,omitempty"`
 }
 
-type Links struct {
-	PR       string   `yaml:",omitempty"`
-	Commit   string   `yaml:",omitempty"`
-	Advisory string   `yaml:",omitempty"`
-	Context  []string `yaml:",omitempty"`
-}
-
 type CVEMeta struct {
 	ID          string `yaml:",omitempty"`
 	CWE         string `yaml:",omitempty"`
@@ -196,9 +189,8 @@ type Report struct {
 	// the above CVEs.
 	GHSAs []string `yaml:",omitempty"`
 
-	Credit          string       `yaml:",omitempty"`
-	DeprecatedLinks Links        `yaml:"links,omitempty"`
-	References      []*Reference `yaml:",omitempty"`
+	Credit     string       `yaml:",omitempty"`
+	References []*Reference `yaml:",omitempty"`
 
 	// CVEMetdata is used to capture CVE information when we want to assign a
 	// CVE ourselves. If a CVE already exists for an issue, use the CVE field
@@ -247,45 +239,7 @@ func Read(filename string) (_ *Report, err error) {
 	if err := d.Decode(&r); err != nil {
 		return nil, fmt.Errorf("yaml.Decode: %v", err)
 	}
-	r.upgrade()
 	return &r, nil
-}
-
-func (r *Report) upgrade() {
-	if u := r.DeprecatedLinks.Advisory; u != "" {
-		r.References = append(r.References, &Reference{
-			Type: ReferenceTypeAdvisory,
-			URL:  u,
-		})
-	}
-	if u := r.DeprecatedLinks.PR; u != "" {
-		r.References = append(r.References, &Reference{
-			Type: ReferenceTypeFix,
-			URL:  u,
-		})
-	}
-	if u := r.DeprecatedLinks.Commit; u != "" {
-		r.References = append(r.References, &Reference{
-			Type: ReferenceTypeFix,
-			URL:  u,
-		})
-	}
-	for _, u := range r.DeprecatedLinks.Context {
-		typ := ReferenceTypeWeb
-		switch {
-		case strings.Contains(u, "/issue/"):
-			typ = ReferenceTypeReport
-		case strings.Contains(u, "/cl/"):
-			typ = ReferenceTypeFix
-		case strings.Contains(u, "https://go.googlesource.com/"):
-			typ = ReferenceTypeFix
-		}
-		r.References = append(r.References, &Reference{
-			Type: typ,
-			URL:  u,
-		})
-	}
-	r.DeprecatedLinks = Links{}
 }
 
 // Write writes r to filename in YAML format.
