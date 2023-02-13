faac2b9c6e8b3034016943d9c35eefd048a5a805
Tatiana Bradley <tatianabradley@google.com>
2023-02-13 16:51:49 -0500

    internal/report: improve ref type inferrence
    Improve "ReferenceType" inference for URLs by allowing more possibilities
    and requiring leading and trailing slashes (to reduce false positives).
    
    Change-Id: Ibee7689f88a277aed2776684e45cae7a3d06ec5a
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/467856
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Tatiana Bradley <tatianabradley@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Tim King <taking@google.com>

--- internal/report/references.go
+++ internal/report/references.go
@@ -6,21 +6,31 @@ package report
 
 import "strings"
 
+func isFix(url string) bool {
+	return strings.Contains(url, "go-review.googlesource.com") ||
+		strings.Contains(url, "/commit/") || strings.Contains(url, "/commits/") ||
+		strings.Contains(url, "/pull/") || strings.Contains(url, "/cl/")
+}
+
+func isIssue(url string) bool {
+	return strings.Contains(url, "/issue/") || strings.Contains(url, "/issues/")
+}
+
+func isAdvisory(url string) bool {
+	return strings.Contains(url, "/advisories/")
+}
+
 // referenceFromUrl creates a new Reference from a url
 // with Type inferred from the contents of the url.
 func referenceFromUrl(url string) *Reference {
 	typ := ReferenceTypeWeb
 	switch {
-	case strings.Contains(url, "go-review.googlesource.com"):
+	case isFix(url):
 		typ = ReferenceTypeFix
-	case strings.Contains(url, "commit"):
-		typ = ReferenceTypeFix
-	case strings.Contains(url, "pull"):
-		typ = ReferenceTypeFix
-	case strings.Contains(url, "pr"):
-		typ = ReferenceTypeFix
-	case strings.Contains(url, "/issue/"):
+	case isIssue(url):
 		typ = ReferenceTypeReport
+	case isAdvisory(url):
+		typ = ReferenceTypeAdvisory
 	}
 	return &Reference{
 		Type: typ,
