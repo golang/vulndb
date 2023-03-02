5cd5d8043272220352811db3b499c0cdee7aaed9
Tatiana Bradley <tatianabradley@google.com>
2023-03-02 17:06:02 -0500

    internal/database/legacydb: add function to check if a v1 and legacy db are the same
    Adds a function, Equivalent, which checks if a database in the legacy
    format is the same as a database in the v1 format. This will be
    used as a pre-deploy check.
    
    For golang/go#58816
    
    Change-Id: I936111882837fc461d2c2819133ac43f5639ab44
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/472882
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>
    Run-TryBot: Tatiana Bradley <tatianabradley@google.com>

---
+++ internal/database/legacydb/equivalent_test.go
@@ -0,0 +1,67 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package legacydb
+
+import (
+	"context"
+	"testing"
+
+	db "golang.org/x/vulndb/internal/database"
+	"golang.org/x/vulndb/internal/gitrepo"
+)
+
+func TestEquivalent(t *testing.T) {
+	ctx := context.Background()
+	testRepo, err := gitrepo.ReadTxtarRepo(testRepoDir, jan2002)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	legacyDir := t.TempDir()
+	err = Generate(ctx, testRepo, legacyDir, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	v1Dir := t.TempDir()
+	v1, err := db.FromRepo(ctx, testRepo)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := v1.Write(v1Dir); err != nil {
+		t.Fatal(err)
+	}
+
+	// Databases created from the same repo should be equivalent.
+	if err := Equivalent(v1Dir, legacyDir); err != nil {
+		t.Error(err)
+	}
+
+	// Equivalent should error because neither of the databases is
+	// valid according to its schema.
+	if err := Equivalent(legacyDir, v1Dir); err == nil {
+		t.Error("Equivalent: got nil, want error")
+	}
+}
+
+func TestCheckSameModulesAndVulns(t *testing.T) {
+	legacy := newTestDB(testOSV1, testOSV2, testOSV3)
+	v1, err := db.New(*testOSV1, *testOSV2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Should error because legacy DB has an extra entry.
+	if err := legacy.checkSameModulesAndVulns(v1); err == nil {
+		t.Error("checkSameModulesAndVulns: got nil, want error")
+	}
+
+	v1.Add(*testOSV3)
+
+	// Should now succeed.
+	if err := legacy.checkSameModulesAndVulns(v1); err != nil {
+		t.Error(err)
+	}
+}
