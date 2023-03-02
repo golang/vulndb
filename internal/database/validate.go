26dafb8702f09a0a580ce13b579e0734f2678c91
Tatiana Bradley <tatianabradley@google.com>
2023-03-02 17:01:04 -0500

    internal/database: add Validate for v1 databases
    Adds functionality to validate that a v1 database is safe to deploy.
    
    For golang/go#58816
    
    Change-Id: I18400553b872afc026daf294e0c9b5c0d3333d98
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/472880
    Run-TryBot: Tatiana Bradley <tatianabradley@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Zvonimir Pavlinovic <zpavlinovic@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

---
+++ internal/database/validate.go
@@ -0,0 +1,57 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package database
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"golang.org/x/vuln/osv"
+	"golang.org/x/vulndb/internal/derrors"
+)
+
+// Validate checks that the databases in newPath and oldPath are
+// both valid databases, and that the database in newPath can
+// be safely deployed on top of the database in oldPath.
+func Validate(newPath, oldPath string) (err error) {
+	derrors.Wrap(&err, "Validate(new=%s, old=%s)", newPath, oldPath)
+
+	new, err := Load(newPath)
+	if err != nil {
+		return err
+	}
+
+	// TODO(https://go.dev/issue/58816): Change this to Load(oldPath)
+	// once the v1 database deploys for the first time.
+	// On the first deploy, the old database will not be valid with
+	// respect to the v1 spec.
+	old, err := RawLoad(filepath.Join(oldPath, idDir))
+	if err != nil {
+		return err
+	}
+
+	return validate(new, old)
+}
+
+// validate checks for deleted files and inconsistent timestamps.
+func validate(new, old *Database) error {
+	newEntriesByID := make(map[string]osv.Entry, len(new.Entries))
+	for _, newEntry := range new.Entries {
+		newEntriesByID[newEntry.ID] = newEntry
+	}
+	for _, oldEntry := range old.Entries {
+		newEntry, ok := newEntriesByID[oldEntry.ID]
+		if !ok {
+			return fmt.Errorf("%s is not present in new database. Use the %q field to delete an entry", oldEntry.ID, "withdrawn")
+		}
+		if newEntry.Published != oldEntry.Published {
+			return fmt.Errorf("%s: published time cannot change (new %s, old %s)", oldEntry.ID, newEntry.Published, oldEntry.Published)
+		}
+		if newEntry.Modified.Before(oldEntry.Modified) {
+			return fmt.Errorf("%s: modified time cannot decrease (new %s, old %s)", oldEntry.ID, newEntry.Modified, oldEntry.Modified)
+		}
+	}
+	return nil
+}
---
+++ internal/database/validate_test.go
@@ -0,0 +1,176 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package database
+
+import (
+	"strings"
+	"testing"
+
+	"golang.org/x/vuln/osv"
+)
+
+func TestValidate(t *testing.T) {
+	small, big, invalid := t.TempDir(), t.TempDir(), t.TempDir()
+	gzip := true
+	if err := txtarToDir(smallTxtar, small, gzip); err != nil {
+		t.Fatal(err)
+	}
+	if err := txtarToDir(validTxtar, big, gzip); err != nil {
+		t.Fatal(err)
+	}
+	if err := txtarToDir(invalidModulesTxtar, invalid, gzip); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("valid", func(t *testing.T) {
+		// Adding more entries is OK.
+		if err := Validate(big, small); err != nil {
+			t.Error(err)
+		}
+	})
+
+	// TODO(https://go.dev/issue/58816): Add a test for an invalid
+	// old DB once we change RawLoad to Load in Validate.
+	failTests := []struct {
+		name string
+		old  string
+		new  string
+	}{
+		{
+			name: "deleted entry",
+			old:  big,
+			new:  small,
+		},
+		{
+			name: "invalid new db",
+			old:  small,
+			new:  invalid,
+		},
+	}
+	for _, test := range failTests {
+		t.Run(test.name, func(t *testing.T) {
+			if err := Validate(test.new, test.old); err == nil {
+				t.Error("expected error, got nil")
+			}
+		})
+	}
+}
+
+func TestValidateInternal(t *testing.T) {
+	successTests := []struct {
+		name string
+		new  []osv.Entry
+		old  []osv.Entry
+	}{
+		{
+			name: "valid updates ok",
+			old: []osv.Entry{{
+				ID:        "GO-1999-0001",
+				Published: jan1999,
+				Modified:  jan1999,
+			}},
+			new: []osv.Entry{{
+				ID:        "GO-1999-0001",
+				Published: jan1999,
+				Modified:  jan2000,
+			}, {
+				ID:        "GO-1999-0002",
+				Published: jan2000,
+				Modified:  jan2000,
+			}},
+		},
+		{
+			name: "same db ok",
+			old: []osv.Entry{{
+				ID:        "GO-1999-0001",
+				Published: jan1999,
+				Modified:  jan1999,
+			}},
+			new: []osv.Entry{{
+				ID:        "GO-1999-0001",
+				Published: jan1999,
+				Modified:  jan1999,
+			}},
+		},
+	}
+	for _, test := range successTests {
+		t.Run(test.name, func(t *testing.T) {
+			new, err := New(test.new...)
+			if err != nil {
+				t.Fatal(err)
+			}
+			old, err := New(test.old...)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := validate(new, old); err != nil {
+				t.Errorf("validate(): unexpected error %v", err)
+			}
+		})
+	}
+
+	failTests := []struct {
+		name    string
+		new     []osv.Entry
+		old     []osv.Entry
+		wantErr string
+	}{
+		{
+			name: "published time changed",
+			old: []osv.Entry{
+				{
+					ID:        "GO-1999-0001",
+					Published: jan1999,
+					Modified:  jan1999,
+				}},
+			new: []osv.Entry{
+				{
+					ID:        "GO-1999-0001",
+					Published: jan2000,
+					Modified:  jan2000,
+				},
+			},
+			wantErr: "published time cannot change",
+		},
+		{
+			name: "deleted entry",
+			old: []osv.Entry{
+				{
+					ID:        "GO-1999-0001",
+					Published: jan1999,
+					Modified:  jan1999,
+				},
+			},
+			wantErr: "GO-1999-0001 is not present in new database",
+		},
+		{
+			name: "modified time decreased",
+			old: []osv.Entry{{
+				ID:       "GO-1999-0001",
+				Modified: jan2000,
+			}},
+			new: []osv.Entry{{
+				ID:       "GO-1999-0001",
+				Modified: jan1999,
+			}},
+			wantErr: "modified time cannot decrease",
+		},
+	}
+	for _, test := range failTests {
+		t.Run(test.name, func(t *testing.T) {
+			new, err := New(test.new...)
+			if err != nil {
+				t.Fatal(err)
+			}
+			old, err := New(test.old...)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := validate(new, old); err == nil || !strings.Contains(err.Error(), test.wantErr) {
+				t.Errorf("validate(): want error containing %q, got %v", test.wantErr, err)
+			}
+		})
+	}
+}
