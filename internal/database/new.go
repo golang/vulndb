ab9aa3a217c83bb612b6fd6eea28d07e9e70db6e
Tatiana Bradley <tatianabradley@google.com>
2023-03-02 16:54:47 -0500

    internal/database: add New function, which creates v1 databases
    Adds a function, New, which creates a database in the v1 schema from
    OSV entries.
    
    For golang/go#58816
    
    Change-Id: I7513419bc151c69ed4a8b48d78b817d50a6d7b90
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/472878
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Damien Neil <dneil@google.com>
    Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
    Run-TryBot: Tatiana Bradley <tatianabradley@google.com>

---
+++ internal/database/new.go
@@ -0,0 +1,95 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package database
+
+import (
+	"fmt"
+
+	"golang.org/x/vuln/osv"
+	"golang.org/x/vulndb/internal/report"
+)
+
+// New creates a new database from the given entries.
+// Errors if there are multiple entries with the same ID.
+func New(entries ...osv.Entry) (*Database, error) {
+	db := &Database{
+		DB:      DBMeta{},
+		Modules: make(ModulesIndex),
+		Vulns:   make(VulnsIndex),
+		Entries: make([]osv.Entry, 0, len(entries)),
+	}
+	for _, entry := range entries {
+		if err := db.Add(entry); err != nil {
+			return nil, err
+		}
+	}
+	return db, nil
+}
+
+// Add adds new entries to a database, erroring if any of the entries
+// is already in the database.
+func (db *Database) Add(entries ...osv.Entry) error {
+	for _, entry := range entries {
+		if err := db.Vulns.add(entry); err != nil {
+			return err
+		}
+		// Only add the entry once we are sure it won't
+		// cause an error.
+		db.Entries = append(db.Entries, entry)
+		db.Modules.add(entry)
+		db.DB.add(entry)
+	}
+	return nil
+}
+
+func (dbi *DBMeta) add(entry osv.Entry) {
+	if entry.Modified.After(dbi.Modified.Time) {
+		dbi.Modified = Time{entry.Modified}
+	}
+}
+
+func (m *ModulesIndex) add(entry osv.Entry) {
+	for _, affected := range entry.Affected {
+		modulePath := affected.Package.Name
+		if _, ok := (*m)[modulePath]; !ok {
+			(*m)[modulePath] = &Module{
+				Path:  modulePath,
+				Vulns: []ModuleVuln{},
+			}
+		}
+		module := (*m)[modulePath]
+		module.Vulns = append(module.Vulns, ModuleVuln{
+			ID:       entry.ID,
+			Modified: Time{entry.Modified},
+			Fixed:    latestFixedVersion(affected.Ranges),
+		})
+	}
+}
+
+func (v *VulnsIndex) add(entry osv.Entry) error {
+	if _, ok := (*v)[entry.ID]; ok {
+		return fmt.Errorf("id %q appears twice in database", entry.ID)
+	}
+	(*v)[entry.ID] = &Vuln{
+		ID:       entry.ID,
+		Modified: Time{entry.Modified},
+		Aliases:  entry.Aliases,
+	}
+	return nil
+}
+
+func latestFixedVersion(ranges osv.Affects) string {
+	var latestFixed report.Version
+	for _, r := range ranges {
+		if r.Type == "SEMVER" {
+			for _, e := range r.Events {
+				if fixed := report.Version(e.Fixed); fixed != "" && latestFixed.Before(fixed) {
+					latestFixed = fixed
+				}
+			}
+		}
+	}
+	return string(latestFixed)
+}
---
+++ internal/database/new_test.go
@@ -0,0 +1,221 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package database
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/vuln/osv"
+)
+
+var (
+	jan1999  = time.Date(1999, 1, 1, 0, 0, 0, 0, time.UTC)
+	jan2000  = time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
+	jan2002  = time.Date(2002, 1, 1, 0, 0, 0, 0, time.UTC)
+	jan2003  = time.Date(2003, 1, 1, 0, 0, 0, 0, time.UTC)
+	testOSV1 = osv.Entry{
+		ID:        "GO-1999-0001",
+		Published: jan1999,
+		Modified:  jan2000,
+		Aliases:   []string{"CVE-1999-1111"},
+		Details:   "Some details",
+		Affected: []osv.Affected{
+			{
+				Package: osv.Package{
+					Name:      "stdlib",
+					Ecosystem: "Go",
+				},
+				Ranges: osv.Affects{
+					osv.AffectsRange{
+						Type: "SEMVER",
+						Events: []osv.RangeEvent{
+							{Introduced: "0"}, {Fixed: "1.1.0"},
+							{Introduced: "1.2.0"},
+							{Fixed: "1.2.2"},
+						}}},
+				DatabaseSpecific: osv.DatabaseSpecific{
+					URL: "https://pkg.go.dev/vuln/GO-1999-0001"},
+				EcosystemSpecific: osv.EcosystemSpecific{
+					Imports: []osv.EcosystemSpecificImport{{Path: "package", Symbols: []string{"Symbol"}}}}},
+		},
+		References: []osv.Reference{
+			{Type: "FIX", URL: "https://example.com/cl/123"},
+		}}
+	testOSV2 = osv.Entry{
+		ID:        "GO-2000-0002",
+		Published: jan2000,
+		Modified:  jan2002,
+		Aliases:   []string{"CVE-1999-2222"},
+		Details:   "Some details",
+		Affected: []osv.Affected{
+			{
+				Package: osv.Package{
+					Name:      "example.com/module",
+					Ecosystem: "Go",
+				},
+				Ranges: osv.Affects{
+					osv.AffectsRange{
+						Type: "SEMVER", Events: []osv.RangeEvent{{Introduced: "0"},
+							{Fixed: "1.2.0"},
+						}}},
+				DatabaseSpecific: osv.DatabaseSpecific{URL: "https://pkg.go.dev/vuln/GO-2000-0002"}, EcosystemSpecific: osv.EcosystemSpecific{
+					Imports: []osv.EcosystemSpecificImport{{Path: "example.com/module/package",
+						Symbols: []string{"Symbol"},
+					}}}}},
+		References: []osv.Reference{
+			{Type: "FIX", URL: "https://example.com/cl/543"},
+		}}
+	testOSV3 = osv.Entry{
+		ID:        "GO-2000-0003",
+		Published: jan2000,
+		Modified:  jan2003,
+		Aliases:   []string{"CVE-1999-3333", "GHSA-xxxx-yyyy-zzzz"},
+		Details:   "Some details",
+		Affected: []osv.Affected{
+			{
+				Package: osv.Package{
+					Name:      "example.com/module",
+					Ecosystem: "Go",
+				},
+				Ranges: osv.Affects{
+					osv.AffectsRange{
+						Type: "SEMVER",
+						Events: []osv.RangeEvent{
+							{Introduced: "0"}, {Fixed: "1.1.0"},
+						}}},
+				DatabaseSpecific: osv.DatabaseSpecific{
+					URL: "https://pkg.go.dev/vuln/GO-2000-0003",
+				},
+				EcosystemSpecific: osv.EcosystemSpecific{Imports: []osv.EcosystemSpecificImport{
+					{
+						Path:    "example.com/module/package",
+						Symbols: []string{"Symbol"},
+					}}}}},
+		References: []osv.Reference{
+			{Type: "FIX", URL: "https://example.com/cl/000"},
+		}}
+	valid = &Database{
+		DB: DBMeta{Modified: Time{jan2003}},
+		Modules: ModulesIndex{
+			"example.com/module": &Module{Path: "example.com/module", Vulns: []ModuleVuln{{ID: "GO-2000-0002", Modified: Time{jan2002}, Fixed: "1.2.0"}, {ID: "GO-2000-0003", Modified: Time{jan2003}, Fixed: "1.1.0"}}}, "stdlib": &Module{Path: "stdlib", Vulns: []ModuleVuln{{ID: "GO-1999-0001", Modified: Time{jan2000}, Fixed: "1.2.2"}}},
+		},
+		Vulns: VulnsIndex{
+			"GO-1999-0001": &Vuln{ID: "GO-1999-0001", Modified: Time{jan2000}, Aliases: []string{"CVE-1999-1111"}}, "GO-2000-0002": &Vuln{ID: "GO-2000-0002", Modified: Time{jan2002}, Aliases: []string{"CVE-1999-2222"}}, "GO-2000-0003": &Vuln{ID: "GO-2000-0003", Modified: Time{jan2003}, Aliases: []string{"CVE-1999-3333", "GHSA-xxxx-yyyy-zzzz"}},
+		},
+		Entries: []osv.Entry{testOSV1, testOSV2, testOSV3}}
+)
+
+func TestNew(t *testing.T) {
+	got, err := New(testOSV1, testOSV2, testOSV3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := valid
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("New: unexpected diff (-want, +got):\n%v", diff)
+	}
+}
+
+func TestLatestFixedVerion(t *testing.T) {
+	tests := []struct {
+		name   string
+		ranges []osv.AffectsRange
+		want   string
+	}{
+		{
+			name:   "empty",
+			ranges: []osv.AffectsRange{},
+			want:   "",
+		},
+		{
+			name: "no fix",
+			ranges: []osv.AffectsRange{{
+				Type: osv.TypeSemver,
+				Events: []osv.RangeEvent{
+					{
+						Introduced: "0",
+					},
+				},
+			}},
+			want: "",
+		},
+		{
+			name: "unsorted",
+			ranges: []osv.AffectsRange{{
+				Type: osv.TypeSemver,
+				Events: []osv.RangeEvent{
+					{
+						Fixed: "1.0.0",
+					},
+					{
+						Introduced: "0",
+					},
+					{
+						Fixed: "0.1.0",
+					},
+					{
+						Introduced: "0.5.0",
+					},
+				},
+			}},
+			want: "1.0.0",
+		},
+		{
+			name: "multiple ranges",
+			ranges: []osv.AffectsRange{{
+				Type: osv.TypeSemver,
+				Events: []osv.RangeEvent{
+					{
+						Introduced: "0",
+					},
+					{
+						Fixed: "0.1.0",
+					},
+				},
+			},
+				{
+					Type: osv.TypeSemver,
+					Events: []osv.RangeEvent{
+						{
+							Introduced: "0",
+						},
+						{
+							Fixed: "0.2.0",
+						},
+					},
+				}},
+			want: "0.2.0",
+		},
+		{
+			name: "pseudoversion",
+			ranges: []osv.AffectsRange{{
+				Type: osv.TypeSemver,
+				Events: []osv.RangeEvent{
+					{
+						Introduced: "0",
+					},
+					{
+						Fixed: "0.0.0-20220824120805-abc",
+					},
+					{
+						Introduced: "0.0.0-20230824120805-efg",
+					},
+					{
+						Fixed: "0.0.0-20240824120805-hij",
+					},
+				},
+			}},
+			want: "0.0.0-20240824120805-hij",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			latestFixedVersion(test.ranges)
+		})
+	}
+}
