294a5723ad93ece970b2837b08812c42743c6c31
Tatiana Bradley <tatianabradley@google.com>
2023-08-02 15:15:14 -0400

    cmd/inspect: add breakdown by year
    In the "inspect" command, display summary of reports & GHSAs by year in
    addition to totals. This allows us to view trends over time.
    
    Change-Id: Ib49017f28b0bbfbb01fb0711703a91b911740cdc
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/515397
    Run-TryBot: Tatiana Bradley <tatianabradley@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Damien Neil <dneil@google.com>

--- cmd/inspect/main.go
+++ cmd/inspect/main.go
@@ -11,13 +11,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 	"text/tabwriter"
 	"time"
 
 	"golang.org/x/exp/maps"
+	"golang.org/x/vulndb/internal/genericosv"
 	"golang.org/x/vulndb/internal/ghsarepo"
 	"golang.org/x/vulndb/internal/gitrepo"
 	"golang.org/x/vulndb/internal/report"
+	"golang.org/x/vulndb/internal/stdlib"
 )
 
 var (
@@ -56,70 +61,162 @@ func main() {
 		}
 	}
 
-	ghsas := c.IDs()
-	summary := summarize(ghsas, reports)
+	ghsas := c.List()
+	overall, byYear := summarize(ghsas, reports)
 
-	fmt.Printf("=== Summary (%s) ===\n", time.Since(start).Truncate(time.Millisecond))
-	fmt.Println()
-	tw := tabwriter.NewWriter(os.Stdout, 2, 4, 1, ' ', 0)
-	fmt.Fprintf(tw, "%s\t%4d\n", "Go reports", len(reports))
-	fmt.Fprintf(tw, "-- %s\t%4d\n", "Regular Go reports", summary.regular)
-	fmt.Fprintf(tw, "-- %s\t%4d\n", "Excluded Go reports", summary.excluded)
-	fmt.Fprintf(tw, "-- %s\t%4d\n", "Go reports with no GHSA (+)", len(summary.noGHSA))
-	fmt.Fprintf(tw, "%s\t%4d\n", "GHSAs affecting Go", len(ghsas))
-	fmt.Fprintf(tw, "-- %s\t%4d\n", "GHSAs not yet in vulndb (++)", len(summary.ghsasNotInVDB))
-	tw.Flush()
-	fmt.Println()
-	fmt.Println("(+) \"Go reports with no GHSA\" are published third-party Go reports with no corresponding GHSA. (This isn't a problem; it's informational only.)")
-	fmt.Println("(++) \"GHSAs not yet in vulndb\" are published GHSAs with no corresponding Go report. There may already be an open issue on the tracker for these.")
-
+	display(overall, byYear)
 	if *detail {
 		fmt.Println("\n=== GHSAs not yet in vulndb ===")
-		for i, g := range summary.ghsasNotInVDB {
-			fmt.Println()
-			fmt.Printf("%d) %s\n", i+1, g)
-			fmt.Printf("https://github.com/advisories/%s\n", g)
-			fmt.Printf("search issue tracker: https://github.com/golang/vulndb/issues?q=is%%3Aissue+%s\n", g)
+		displayGHSAs(overall.ghsasNotInVDB)
+	}
+
+	fmt.Printf("\n%s\n", time.Since(start).Truncate(time.Millisecond))
+}
+
+func display(overall *summary, byYear map[int]*summary) {
+	tw := tabwriter.NewWriter(os.Stdout, 2, 4, 2, ' ', 0)
+	years := maps.Keys(byYear)
+	sort.Sort(sort.Reverse(sort.IntSlice(years))) // sort descending
+
+	headings := func() {
+		fmt.Fprintf(tw, "\t%s", "Total")
+		for _, year := range years {
+			fmt.Fprintf(tw, "\t%4d", year)
 		}
+		fmt.Fprintf(tw, "\n")
+		fmt.Fprintf(tw, "\t%s", "-----")
+		for range years {
+			fmt.Fprintf(tw, "\t%4s", "----")
+		}
+		fmt.Fprintf(tw, "\n")
+	}
+	data := func(desc string, indent int, getData func(s *summary) int) {
+		var indentS string
+		if indent > 0 {
+			indentS = strings.Repeat("--", indent) + " "
+		}
+		fmt.Fprintf(tw, "%s%s\t%4d", indentS, desc, getData(overall))
+		for _, year := range years {
+			fmt.Fprintf(tw, "\t%4d", getData(byYear[year]))
+		}
+		fmt.Fprintf(tw, "\n")
+	}
+	newline := func() {
+		fmt.Fprintf(tw, "%s\n", strings.Repeat("\t", len(years))) // preserve tab formatting
+	}
+
+	// Summary of Go reports by year.
+	headings()
+	data("Go reports", 0, func(s *summary) int { return s.reports })
+	data("Regular reports", 1, func(s *summary) int { return s.regular })
+	data("Excluded reports", 1, func(s *summary) int { return s.excluded })
+	for _, er := range report.ExcludedReasons {
+		data(string(er), 2, func(s *summary) int { return s.excludedByReason[er] })
+	}
+	data("Reports with no GHSA (+)", 1, func(s *summary) int { return s.noGHSA })
+	data("Stdlib, toolchain and x/ reports", 1, func(s *summary) int { return s.firstParty })
+
+	// Summary of GHSAs by year.
+	newline()
+	headings()
+	data("GHSAs affecting Go", 0, func(s *summary) int { return s.ghsas })
+	data("GHSAs not yet in vulndb (++)", 1, func(s *summary) int { return len(s.ghsasNotInVDB) })
+
+	// Additional context.
+	newline()
+	fmt.Fprintln(tw, "(+) \"Go reports with no GHSA\" are published third-party Go reports\nwith no corresponding GHSA. (This isn't a problem; it's informational only.)")
+	fmt.Fprintln(tw, "(++) \"GHSAs not yet in vulndb\" are published GHSAs with no corresponding\nGo report. There may already be an open issue on the tracker for these.")
+
+	tw.Flush()
+}
+
+func displayGHSAs(ghsas []string) {
+	for i, g := range ghsas {
+		fmt.Println()
+		fmt.Printf("%d) %s\n", i+1, g)
+		fmt.Printf("https://github.com/advisories/%s\n", g)
+		fmt.Printf("search issue tracker: https://github.com/golang/vulndb/issues?q=is%%3Aissue+%s\n", g)
 	}
 }
 
 type summary struct {
-	regular, excluded int
-	ghsasNotInVDB     []string
-	noGHSA            []string
+	reports, regular, excluded, noGHSA, firstParty int
+	ghsas                                          int
+	ghsasNotInVDB                                  []string
+	excludedByReason                               map[report.ExcludedReason]int
 }
 
-func summarize(ghsas []string, reports []*report.Report) *summary {
-	ghsasWithReport := make(map[string]bool)
-	var noGHSA []string
-	var regular, excluded int
+func newSummary() *summary {
+	return &summary{
+		excludedByReason: make(map[report.ExcludedReason]int),
+	}
+}
 
+func summarize(ghsas []*genericosv.Entry, reports []*report.Report) (*summary, map[int]*summary) {
+	overall := newSummary()
+	byYear := make(map[int]*summary)
+
+	ghsasWithReport := make(map[string]bool)
 	for _, r := range reports {
-		if r.IsExcluded() {
-			excluded++
-		} else {
-			regular++
+		year, err := strconv.Atoi(strings.Split(r.ID, "-")[1])
+		if err != nil {
+			panic(err)
 		}
+		if _, ok := byYear[year]; !ok {
+			byYear[year] = newSummary()
+		}
+		yearSummary := byYear[year]
+
+		overall.reports++
+		yearSummary.reports++
+
+		if isFirstParty(r) {
+			overall.firstParty++
+			yearSummary.firstParty++
+		}
+
+		if r.IsExcluded() {
+			overall.excluded++
+			overall.excludedByReason[r.Excluded]++
+
+			yearSummary.excluded++
+			yearSummary.excludedByReason[r.Excluded]++
+		} else {
+			overall.regular++
+			yearSummary.regular++
+		}
+
 		if len(r.GHSAs) == 0 && r.CVEMetadata == nil {
-			noGHSA = append(noGHSA, r.ID)
+			overall.noGHSA++
+			yearSummary.noGHSA++
 		}
 		for _, ghsa := range r.GHSAs {
 			ghsasWithReport[ghsa] = true
 		}
 	}
 
-	var ghsasNotInVDB []string
 	for _, ghsa := range ghsas {
-		if _, ok := ghsasWithReport[ghsa]; !ok {
-			ghsasNotInVDB = append(ghsasNotInVDB, ghsa)
+		year := ghsa.Published.Year()
+		if _, ok := byYear[year]; !ok {
+			byYear[year] = newSummary()
+		}
+		yearSummary := byYear[year]
+		overall.ghsas++
+		yearSummary.ghsas++
+		if _, ok := ghsasWithReport[ghsa.ID]; !ok {
+			overall.ghsasNotInVDB = append(overall.ghsasNotInVDB, ghsa.ID)
+			yearSummary.ghsasNotInVDB = append(yearSummary.ghsasNotInVDB, ghsa.ID)
 		}
 	}
 
-	return &summary{
-		regular:       regular,
-		excluded:      excluded,
-		ghsasNotInVDB: ghsasNotInVDB,
-		noGHSA:        noGHSA,
+	return overall, byYear
+}
+
+func isFirstParty(r *report.Report) bool {
+	for _, m := range r.Modules {
+		if stdlib.IsStdModule(m.Module) || stdlib.IsCmdModule(m.Module) || stdlib.IsXModule(m.Module) {
+			return true
+		}
 	}
+	return false
 }
