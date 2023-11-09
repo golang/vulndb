9f2306b6e2bbca29fb13043b93c78e2dcc9b529d
Jonathan Amsterdam <jba@google.com>
2023-11-09 16:44:37 -0500

    cmd/forks: various improvements
    - Add help.
    
    - Use new DB format that fixes some bugs.
    
    - Print matching modules with their score.
    
    Change-Id: Ie12e2ca69074764f3263daa747683eabb9220764
    Reviewed-on: https://go-review.googlesource.com/c/vulndb/+/541256
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>

--- cmd/forks/main.go
+++ cmd/forks/main.go
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build go1.21
+
 // Command forks determines if Go modules are similar.
 package main
 
 import (
+	"cmp"
 	"context"
 	"encoding/gob"
 	"errors"
@@ -14,11 +17,23 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"slices"
 	"strings"
 	"time"
 )
 
 func main() {
+	out := flag.CommandLine.Output()
+	flag.Usage = func() {
+		fmt.Fprintf(out, "usage: forks [ PATH | PATH@VERSION ] ...\n")
+		fmt.Fprintf(out, "Print potential forks for each module path or module path @ version.\n")
+		fmt.Fprintf(out, "Each fork is preceded by its score. Scores range from 0 to 10, with 10 meaning most\n")
+		fmt.Fprintf(out, "similar. Only matches with scores of at least 6 are printed.\n")
+		fmt.Fprintf(out, "Scores are approximations that are based on partial data (not the full module content),\n")
+		fmt.Fprintf(out, "so even a score of 10 does not mean that the modules are identical.\n")
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
 	if err := run(ctx); err != nil {
@@ -26,16 +41,25 @@ func main() {
 	}
 }
 
-type ForksDB struct {
+type Forks struct {
 	Comment   string
 	Timestamp time.Time
-	// Mapping from int to module@version
-	Modules []string
-	// Equivalence classes
-	Classes [][]int
+	MinScore  int             // smallest score that is stored
+	Modules   []string        // mapping from int to module@version
+	Matches   map[int][]Score // from module ID to matches and their scores
+
+}
+
+type Score struct {
+	Module int // index into Forks.Modules
+	Score  int // 0 - 10
 }
 
 func run(ctx context.Context) error {
+	if flag.NArg() == 0 {
+		flag.Usage()
+		return nil
+	}
 	dbFilename := os.Getenv("FORKSDB")
 	if dbFilename == "" {
 		return errors.New("must set FORKSDB to file")
@@ -46,58 +70,47 @@ func run(ctx context.Context) error {
 	}
 	defer f.Close()
 	dec := gob.NewDecoder(f)
-	var db ForksDB
+	var db Forks
 	if err := dec.Decode(&db); err != nil {
 		return err
 	}
 
-	// Build a map from module@version to equivalence class.
-	moduleToClass := map[string][]int{}
-	for _, c := range db.Classes {
-		for _, id := range c {
-			moduleToClass[db.Modules[id]] = c
-		}
-	}
+	// Build a map from path@version and path to ID.
+	modsToIDs := buildIndex(db.Modules)
 
-	// Build a map from a module path to all its versions.
-	pathToVersions := map[string][]string{}
-	for _, mv := range db.Modules {
-		path, version, found := strings.Cut(mv, "@")
-		if !found {
-			return fmt.Errorf("bad module@version: %q", mv)
-		}
-		pathToVersions[path] = append(pathToVersions[path], version)
-	}
-
-	printForks := func(mv string) {
-		c := moduleToClass[mv]
-		if c == nil {
-			fmt.Printf("%s: no forks\n", mv)
-		} else {
-			fmt.Printf("%s:\n", mv)
-			for _, id := range c {
-				mv2 := db.Modules[id]
-				if mv2 != mv {
-					fmt.Printf("    %s\n", mv2)
-				}
-			}
-			fmt.Println()
-		}
-	}
-
+	// Print the forks for each arg.
 	for _, arg := range flag.Args() {
-		if strings.ContainsRune(arg, '@') {
-			printForks(arg)
-		} else { // treat arg as a module path
-			versions := pathToVersions[arg]
-			if len(versions) == 0 {
-				fmt.Printf("%s is not a known module path\n", arg)
-			} else {
-				for _, v := range versions {
-					printForks(arg + "@" + v)
+		if ids, ok := modsToIDs[arg]; ok {
+			for _, id := range ids {
+				fmt.Printf("%s\n", db.Modules[id])
+				matches := db.Matches[id]
+				slices.SortFunc(matches, func(s1, s2 Score) int {
+					if c := cmp.Compare(s1.Score, s2.Score); c != 0 {
+						return c
+					}
+					return cmp.Compare(db.Modules[s1.Module], db.Modules[s2.Module])
+				})
+				for _, m := range matches {
+					fmt.Printf("    %2d  %s\n", m.Score, db.Modules[m.Module])
 				}
 			}
+		} else {
+			fmt.Printf("%s: no forks\n", arg)
 		}
 	}
 	return nil
+}
+
+// buildIndex builds a map from "path@version" and "path" to IDs.
+func buildIndex(mods []string) map[string][]int {
+	modsToIDs := map[string][]int{}
+	for i, mv := range mods {
+		path, _, found := strings.Cut(mv, "@")
+		if !found {
+			panic(fmt.Errorf("no '@' in %s", mv))
+		}
+		modsToIDs[mv] = append(modsToIDs[mv], i)
+		modsToIDs[path] = append(modsToIDs[path], i)
+	}
+	return modsToIDs
 }
